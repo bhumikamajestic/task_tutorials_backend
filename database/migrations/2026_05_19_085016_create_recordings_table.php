8acd85f284e329e085c719761042d101d2d9ae73
Dakshprajapat1212 <dakshprajapat1212@gmail.com>
2026-05-19 14:35:10 +0530

    updated migrations

--- database/migrations/2026_05_19_085016_create_recordings_table.php
+++ database/migrations/2026_05_19_085016_create_recordings_table.php
@@ -8,21 +8,35 @@ return new class extends Migration
 {
     /**
      * Run the migrations.
-     *
-     * @return void
      */
     public function up()
     {
         Schema::create('recordings', function (Blueprint $table) {
+
+            // Primary Key
             $table->id();
+
+            // Foreign Key -> classes table
+            $table->foreignId('class_id')
+                  ->constrained('classes')
+                  ->onDelete('cascade');
+
+            // Recording Topic
+            $table->string('topic', 100);
+
+            // Duration in minutes
+            $table->integer('duration');
+
+            // Video URL
+            $table->string('video_link', 100);
+
+            // created_at & updated_at
             $table->timestamps();
         });
     }
 
     /**
      * Reverse the migrations.
-     *
-     * @return void
      */
     public function down()
     {
